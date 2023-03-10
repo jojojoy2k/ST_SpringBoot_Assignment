85bc33230bda47169eb126f45c2361e741d8ee9c
@jojojoy2k <jojojoy2k@gmail.com>
2023-03-10 06:30:15 -0700

    -Fixed  -Test task from command line  -War build

--- src/test/java/com/app/user/UserManagementApplicationTests.java
+++ src/test/java/com/app/user/UserManagementApplicationTests.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class UserManagementApplicationTests {
 
 	@Test
 	void contextLoads() {
